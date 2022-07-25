f09209a333ff5e00e48b67a48d29ad776409441f
sargis-sh <sargis.shahinyan.y@tumo.org>
2022-07-25 13:05:34 +0400

    Esi en logini pahna

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView,View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './Components/HomeScreen/HomeScreen';
@@ -7,56 +7,24 @@ import Profile from './Components/Profile/Profile';
 import Messenger from './Components/MessengerScreen/Messenger';
 
 import { MaterialCommunityIcons,Ionicons,MaterialIcons } from '@expo/vector-icons'; 
+import { Register } from './src/Register';
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#e3e8e4',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} options={
-          {
-            title: 'Home',
-            tabBarIcon: () => {
-              return (
-                <Ionicons name="ios-home-outline" size={24} color="black" />
-              );
-            },
-            tabBarActiveTintColor: 'gray',
-          }
-        } />
-        <Tab.Screen name="Messenger" component={Messenger} options={
-          {
-            title: 'Messenger',
-            tabBarIcon: () => {
-              return (
-                <MaterialIcons name="messenger-outline" size={24} color="black" />
-              );
-            },
-            tabBarActiveTintColor: 'gray',
-          }
-        }/>
-        <Tab.Screen name="Profile" component={Profile} options={
-          {
-            title: 'Profile',
-            tabBarIcon: () => {
-              return (
-                <MaterialCommunityIcons name="account-circle-outline" size={24} color="black" />
-              );
-            },
-            tabBarActiveTintColor: 'gray',
-          }
-        }/>
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <Register />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'aqua',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
